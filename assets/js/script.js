ee1bf1153cc88c7166765d2f5644f390359194a5
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-10-09 21:38:49 +0530

    introducing const variables

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const password = document.getElementById('password');
+const length = 12;
+
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*()_+~|}{[]></-=';
