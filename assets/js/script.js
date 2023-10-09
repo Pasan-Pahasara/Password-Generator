94120e12512d6a5392a59c39b399558815c1fcbc
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-10-09 21:54:44 +0530

    implement generatePassword function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
-const password = document.getElementById('password');
+const password = document.getElementById("password");
 const length = 12;
 
-const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-const numbers = '0123456789';
-const symbols = '!@#$%^&*()_+~|}{[]></-=';
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+~|}{[]></-=";
+
+function generatePassword() {
+  let passwordValue = "";
+  let characters = upperCase + lowerCase + numbers + symbols;
+  for (let i = 0; i < length; i++) {
+    passwordValue += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  password.value = passwordValue;
+}
